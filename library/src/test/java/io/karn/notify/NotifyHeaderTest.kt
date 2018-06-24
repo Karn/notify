3aa0a39ec72023d097a3fb7a5f73611a56e142fa
Karn Saheb <saheb.karn@live.com>
2018-06-23 23:08:07 -0400

    Fix Jacoco agent.

--- library/src/test/java/io/karn/notify/NotifyHeaderTest.kt
+++ library/src/test/java/io/karn/notify/NotifyHeaderTest.kt
@@ -1,7 +1,7 @@
 package io.karn.notify
 
 import android.support.v4.app.NotificationCompat
-import junit.framework.Assert
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
