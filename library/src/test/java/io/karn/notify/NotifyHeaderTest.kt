19f50bc804c2793b0609b4e9428db941f49cdd34
Karn Saheb <saheb.karn@live.com>
2018-06-23 21:42:22 -0400

    Dependency upgrades and reactivation of Ignored Test.

--- library/src/test/java/io/karn/notify/NotifyHeaderTest.kt
+++ library/src/test/java/io/karn/notify/NotifyHeaderTest.kt
@@ -2,7 +2,6 @@ package io.karn.notify
 
 import android.support.v4.app.NotificationCompat
 import junit.framework.Assert
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -11,7 +10,6 @@ import org.robolectric.RobolectricTestRunner
 class NotifyHeaderTest : NotifyTestBase() {
 
     @Test
-    @Ignore
     fun defaultHeaderTest() {
         val notification = Notify.with(this.context)
                 .content {
@@ -23,9 +21,7 @@ class NotifyHeaderTest : NotifyTestBase() {
 
         Assert.assertEquals(context.resources.getDrawable(R.drawable.ic_app_icon, context.theme), notification.smallIcon.loadDrawable(context))
         // Validating color is not reliable. The notification color is randomly returned as ##FFAAAAAA
-        Assert.assertEquals(
-                String.format("#%06X", context.resources.getColor(R.color.notification_header_color, context.theme)),
-                String.format("#%06X", 0xFFFFFFFF and notification.color.toLong()))
+        Assert.assertEquals(0x4A90E2, 0xFFFFFF and notification.color)
         Assert.assertEquals(null, notification.extras.getCharSequence(NotificationCompat.EXTRA_SUB_TEXT))
         Assert.assertTrue(notification.extras.getBoolean(NotificationCompat.EXTRA_SHOW_WHEN))
     }
@@ -52,9 +48,7 @@ class NotifyHeaderTest : NotifyTestBase() {
                 .build()
 
         Assert.assertEquals(context.resources.getDrawable(testIcon, context.theme), notification.smallIcon.loadDrawable(context))
-        Assert.assertEquals(
-                String.format("#%06X", testColor),
-                String.format("#%06X", 0xFFFFFF and notification.color))
+        Assert.assertEquals(testColor, 0xFFFFFF and notification.color)
         Assert.assertEquals(testHeaderText, notification.extras.getCharSequence(NotificationCompat.EXTRA_SUB_TEXT))
         Assert.assertEquals(testShowTimestamp, notification.extras.getBoolean(NotificationCompat.EXTRA_SHOW_WHEN))
     }
